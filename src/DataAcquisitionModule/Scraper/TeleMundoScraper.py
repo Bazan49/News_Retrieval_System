62e7e158482ef33dcf3688c67ec5df8731b14567
Bazan49 <aixabazan0705@gmail.com>
2026-03-08 12:46:56 -0400

    feat(scrapers/telemundo): extract authors from article header

---
+++ src/DataAcquisitionModule/Scraper/TeleMundoScraper.py
@@ -0,0 +1,49 @@
+import re
+from baseScraper import BaseScraper
+from newspaper import Article
+import re
+
+class TeleMundoScraper(BaseScraper):
+
+    def extract_authors(self, article):
+
+        """Extrae los autores de artículos de TeleMundo buscando patrones comunes en el texto"""
+
+        inicio = article.text[:300]  # solo el inicio del artículo
+
+        match = re.search(r"^Por ([A-ZÁÉÍÓÚÑ][A-Za-zÁÉÍÓÚÑ, y]+)", inicio)
+
+        if not match:
+            return article.authors
+
+        authors = match.group(1)
+
+        authors = authors.replace(" y ", ", ")
+        authors_list = [a.strip() for a in authors.split(",")]
+
+        return authors_list
+
+    def extract(self, url, html):
+
+        article = Article(url, language="es")
+        article.download()
+        article.parse()
+
+        title = article.title
+        content = article.text
+        authors = self.extract_authors(article)
+        date = article.publish_date
+
+        return {
+            "title": title,
+            "content": content,
+            "authors": authors,
+            "date": date
+        }
+    
+if __name__ == "__main__":
+    scraper =  TeleMundoScraper()
+    
+    url = "https://www.telemundo.com/noticias/noticias-telemundo/economia/cinco-senales-de-que-la-economia-no-esta-precisamente-rugiendo-como-as-rcna262307"
+    result = scraper.process_url(url, "telemundo")
+    
