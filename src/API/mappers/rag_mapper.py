7306b5f6d3abff4de72ad08cd6378e76b46583aa
Bazan49 <aixabazan0705@gmail.com>
2026-05-05 11:05:08 -0400

    feat(api): add mapper to convert RAGResult to RAGResponseSchema

---
+++ src/API/mappers/rag_mapper.py
@@ -0,0 +1,12 @@
+from src.RAG_Module.Domain.rag_result import RAGResult
+from src.API.schemas.search_response import RAGResponseSchema
+from src.API.mappers.search_mapper import map_to_search_result_item
+
+def map_to_rag_response(rag_result: RAGResult) -> RAGResponseSchema:
+    """Convierte RAGResult (dominio) a RAGResponseSchema (API)."""
+    sources = [map_to_search_result_item(doc) for doc in rag_result.sources]
+    return RAGResponseSchema(
+        query=rag_result.query,
+        answer=rag_result.answer,
+        sources=sources
+    )
